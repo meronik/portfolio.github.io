8bdff6cddf5529cb72cb34186d736f52581a4e59
meronik <meronika.a2003@gmail.com>
2024-03-20 22:58:47 +0530

    changes

--- src/component/Resume.js
+++ src/component/Resume.js
@@ -1,13 +1,12 @@
  
 import React from 'react'
-// import {Document,Page} from 'react-pdf'
-import pdfFile from './Meronika.pdf'
+
 
 var Resume=()=>{
     return(
        
         <div style={{display:"flex",justifyContent:"center",minHeight:"100vh"}}>
-        <iframe src="https://drive.google.com/file/d/1D3Gl2T7ISJH0xhqXYDOIKPt9vZ7UfjOr/preview" width="900"  allow="autoplay"></iframe></div>
+        <iframe src="https://drive.google.com/file/d/1D3Gl2T7ISJH0xhqXYDOIKPt9vZ7UfjOr/preview" title='resume' width="900"  allow="autoplay"></iframe></div>
         
     );
 }
